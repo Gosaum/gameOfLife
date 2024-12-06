7f003b9330f8492fed5322a3919a544bbe848509
Sp2ctrale <clement.richard@viacesi.fr>
2024-12-06 10:20:57 +0100

    Delete code directory

--- code/standardcell.cpp
+++
@@ -1,3 +0,0 @@
-#include "standardcell.hpp"
-
-StandardCell::StandardCell(bool state, int x, int y): Cell(state,x,y) {};
--- code/standardgrid.cpp
+++
@@ -1,7 +0,0 @@
-#include "standardgrid.hpp"
-
-StandardGrid::StandardGrid():Grid(){};
-
-StandardGrid::StandardGrid(int n, int p):Grid(n,p){};
-
-StandardGrid::StandardGrid(int n, int p,const vector<string>& data):Grid(n,p,data){};
